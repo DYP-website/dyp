1c72bf9df8f18e938e4b5c387758f777314c1568
DYP-website <arvin.bokhoree@live.it>
2026-06-16 18:35:12 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+window.DYP_FIREBASE = {
+  enabled: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDYmaVa-ukIrhoGhM1yAevvEwRZBP6818s",
+    authDomain: "dyp-5db75.firebaseapp.com",
+    projectId: "dyp-5db75",
+    storageBucket: "dyp-5db75.firebasestorage.app",
+    messagingSenderId: "553803049106",
+    appId: "1:553803049106:web:b522c4742075aaef241e4b"
+  }
+};
+
+window.DYP_ADMIN_EMAIL = "yogesh.bokhoree@gmail.com";
